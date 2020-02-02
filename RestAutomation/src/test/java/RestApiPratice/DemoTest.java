96fc857fae1f79645e39260ee31dcea76157f6a2
arije <arije@DESKTOP-1V1T2JS>
2020-02-02 11:52:04 +0530

    Conflict generated by Arijeet

--- RestAutomation/src/test/java/RestApiPratice/DemoTest.java
+++ RestAutomation/src/test/java/RestApiPratice/DemoTest.java
@@ -8,15 +8,15 @@ public class DemoTest {
 	public void demoRun()
 	{
 		System.out.println("Demo Runningg");
-
 		System.out.println("hi");
 		System.out.println("Why  Rejection");
-
-
 		System.out.println("conflicts testingss");
-		
 		System.out.println("Hellooo");
-
-
+	}
+	@Test
+	public void DemoTest_2()
+	{
+		int r = 5;
+		double area = r*r;
 	}
 }

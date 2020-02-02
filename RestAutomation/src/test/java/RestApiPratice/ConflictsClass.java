462226970e3e3a7831845b70e50bf47295728d59
Sanjeeb <Sanjeeb@DESKTOP-G5S1Q7S>
2020-02-02 11:53:05 +0530

    Updated by Shreya

---
+++ RestAutomation/src/test/java/RestApiPratice/ConflictsClass.java
@@ -0,0 +1,17 @@
+package RestApiPratice;
+
+import org.testng.annotations.Test;
+
+public class ConflictsClass {
+
+ 
+		@Test
+		public void con()
+		{
+			System.out.println("Print");
+			System.out.println("Message");
+		}
+
+	}
+
+
